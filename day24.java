3b7f55cfdbbbd3f81af67d888ac29c0df67fd80e
Mokshada Naphade <mokshadanaphade@gmail.com>
2025-09-07 23:04:09 +0530

    Create day24.java

---
+++ day24.java
@@ -0,0 +1,42 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Day24 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        return (left != null) ? left : right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        Day24 finder = new Day24();
+        TreeNode p = root.left;      // Node with value 5
+        TreeNode q = root.right;     // Node with value 1
+
+        TreeNode lca = finder.lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor: " + lca.val);
+    }
+}
